89eb10601fc029eb4256baad4bf18ec110bc572d
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-21 17:28:44 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,26 @@
+def instantiate_new_array
+  array = []
+  return array
+end 
+def array_with_two_elements
+  arr = ["dog", "cat"]
+  return arr
+end 
+def first_element(array)
+  return array.first
+end 
+def third_element(array)
+  return array[2]
+end 
+def last_element(array)
+  return array.last
+end 
+def first_element_with_array_methods(array)
+  return array.shift
+end 
+def last_element_with_array_methods(array)
+  return array.pop
+end 
+def length_of_array(array)
+  return array.length
+end 
